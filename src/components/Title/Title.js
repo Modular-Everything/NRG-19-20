2906e8432b90fe0d5b6a3ee9cfda30f6e697c38d
Chrish <chrish@chrishs-mbp.home>
2020-01-30 07:04:29 +0000

    [+] Add some rudamentalory styling

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import tw from 'tailwind.macro';
+import styled from '@emotion/styled';
+
+const TitleForCards = styled.div`
+  ${tw`
+    text-3xl
+    font-bold
+    leading-relaxed
+  `};
+`;
 
 const Title = props => {
   const { is: title } = props;
-
-  return <h2>{title}</h2>;
+  return <TitleForCards>{title}</TitleForCards>;
 };
 
 Title.propTypes = {
