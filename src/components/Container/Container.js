90ede4cb80cc7d140026c257a42fc836af3e4744
Chrish <chrish@chrishs-mbp.home>
2020-02-02 18:39:39 +0000

    [+] Add responsive container

---
+++ src/components/Container/Container.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import tw from 'tailwind.macro';
+import styled from '@emotion/styled';
+
+// ============================================================================
+
+const Container = ({ children }) => {
+  const breakpoints = [640, 768, 1024, 1280];
+  const PageContainer = styled.section`
+    ${tw`
+      mx-auto
+      px-4
+      border
+      border-green-400
+    `}
+    ${breakpoints.map(
+      minWidth =>
+        `@media(min-width: ${minWidth}px) { max-width: ${minWidth}px }`
+    )}
+  `;
+
+  return <PageContainer>{children}</PageContainer>;
+};
+
+// ============================================================================
+
+Container.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+// ============================================================================
+
+export default Container;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
+import Container from '../components/Container';
 import CardsWrapper from '../components/CardsWrapper';
 import Card from '../components/Card';
 
+// ============================================================================
+
 export default () => (
   <Layout>
-    <CardsWrapper>
-      <Card
-        title="Title goes here"
-        subtitle="Sub-Title"
-        cta="Read More"
-        link="/1"
-      />
-      <Card link="/2" />
-      <Card link="/3" />
-      <Card link="/4" />
-    </CardsWrapper>
+    <Container>
+      <CardsWrapper>
+        <Card
+          title="Title goes here"
+          subtitle="Sub-Title"
+          cta="Read More"
+          link="/1"
+        />
+        <Card link="/2" />
+        <Card link="/3" />
+        <Card link="/4" />
+      </CardsWrapper>
+    </Container>
   </Layout>
 );
