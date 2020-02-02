bea576ddd8d40a1018adcbf37efca06617f1fea4
Chrish <chrish@chrishs-mbp.home>
2020-02-02 18:37:20 +0000

    [~] Fix linting errors

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import tw from 'tailwind.macro'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import tw from 'tailwind.macro';
 
-const PageContainer = styled.div`
+// ============================================================================
+
+const PageContainer = styled.main`
   ${tw`
     flex items-center justify-center h-screen w-full bg-gray-100
   `}
-`
+`;
 
-const Layout = ({ children }) => <PageContainer>{children}</PageContainer>
+// ============================================================================
 
-export default Layout
+const Layout = ({ children }) => <PageContainer>{children}</PageContainer>;
+
+// ============================================================================
+
+Layout.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+// ============================================================================
+
+export default Layout;
