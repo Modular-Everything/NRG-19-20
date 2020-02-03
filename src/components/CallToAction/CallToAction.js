2f7d25787e5ad8e350041c2343e969bd65e8db22
n0k5 <hey@chrish.design>
2020-02-03 13:21:07 +0000

    [+] Append red arrow

--- src/components/CallToAction/CallToAction.js
+++ src/components/CallToAction/CallToAction.js
@@ -3,22 +3,46 @@ import PropTypes from 'prop-types';
 import tw from 'tailwind.macro';
 import styled from '@emotion/styled';
 
+import Arrow from '../../../static/images/arrow.svg';
+
+// ============================================================================
+
 const CallToAction = props => {
   const { is: cta } = props;
-  return <StyledCTA>{cta}</StyledCTA>;
+  return (
+    <StyledCTA>
+      {cta}
+      <span>
+        <img src={Arrow} alt="->" />
+      </span>
+    </StyledCTA>
+  );
 };
 
-const StyledCTA = styled.span`
+// ============================================================================
+
+const StyledCTA = styled.div`
   ${tw`
     font-medium
     text-sm
+    flex
+    flex-row
+    items-center
   `}
   color: var(--color-brand);
   font-family: 'Simplon BP', -apple-system, 'Helvetica Neue', sans-serif;
+
+  & span {
+    ${tw`ml-2`}
+  }
 `;
+
+// ============================================================================
 
 CallToAction.propTypes = {
   is: PropTypes.string.isRequired,
 };
+
+// ============================================================================
 
 export default CallToAction;
