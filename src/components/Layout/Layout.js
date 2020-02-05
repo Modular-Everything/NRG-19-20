5c6b24eafb6dc626028ea13f2c259a07cb018fae
n0k5 <hey@chrish.design>
2020-02-05 21:16:01 +0000

    [+] Add meta data as props

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import PropTypes from 'prop-types';
+import tw from 'tailwind.macro';
+import styled from '@emotion/styled';
+import { Helmet } from 'react-helmet';
+
+import Header from '../Header';
 
 // ============================================================================
 
-const Layout = ({ children }) => <main>{children}</main>;
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query Meta {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  const SiteTitle = data.site.siteMetadata.title;
+
+  return (
+    <>
+      <Helmet>
+        <title>{SiteTitle}</title>
+      </Helmet>
+      <Header siteName={SiteTitle} />
+      <StyledMain>{children}</StyledMain>
+    </>
+  );
+};
+
+// ============================================================================
+
+const StyledMain = styled.main`
+  ${tw`pt-4`}
+`;
 
 // ============================================================================
 
