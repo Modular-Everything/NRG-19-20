b029b3ea64b77a67aa48869a35481b57dcb78d41
Chrish <chrish@Chrishs-MacBook-Pro.local>
2020-02-08 15:39:02 +0000

    [~] Further Header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,20 +5,104 @@ import tw from 'tailwind.macro';
 import styled from '@emotion/styled';
 
 import Container from '../Container';
+import Bolt from '../../../static/images/bolt.svg';
 
 // ============================================================================
 
-const Contact = () => <Container>Contact is open</Container>;
+const Line = styled.span`
+  ${tw`h-px w-full block absolute`}
+  background-color: var(--color-black-primary);
+`;
+
+const Plus = styled.div`
+  ${tw`relative`}
+  width: 16px;
+  height: 16px;
+  ${props => props.isOpen && 'transform: rotate(225deg);'}
+
+  & span {
+    top: 8px;
+    ${props => props.isOpen && 'background: var(--color-white-primary)'}
+  }
+
+  & span:first-of-type {
+    transform: rotate(90deg);
+  }
+`;
+
+const Burger = styled.div`
+  ${tw`relative`}
+  width: 32px;
+  height: 32px;
+
+  & span {
+    top: 14px;
+    ${props => props.isOpen && 'background: var(--color-white-primary)'}
+    ${props => props.isOpen && 'background: var(--color-white-primary)'}
+  }
+
+  & span:last-of-type {
+    top: 22px;
+  }
+`;
 
 // ============================================================================
 
-const Navigation = () => <Container>Navigation is open</Container>;
+const Contact = () => (
+  <>
+    <div>
+      <h3>Work with you</h3>
+      <a href="mailto:solutions@madewithnrg.com">
+        solutions
+        <br />
+        @madewithnrg.com
+      </a>
+    </div>
+    <div>
+      <h3>Work with us</h3>
+      <a href="mailto:talent@madewithnrg.com">
+        talent
+        <br />
+        @madewithnrg.com
+      </a>
+    </div>
+    <div>
+      <h3>Everything else</h3>
+      <a href="mailto:questions@madewithnrg.com">
+        questions
+        <br />
+        @madewithnrg.com
+      </a>
+    </div>
+  </>
+);
+const ContactButton = ({ isOpen }) => (
+  <Plus isOpen={isOpen}>
+    <Line />
+    <Line />
+  </Plus>
+);
+
+const Navigation = () => (
+  <>
+    <div>
+      <img src={Bolt} alt="" />
+    </div>
+  </>
+);
+const NavigationButton = ({ isOpen }) => (
+  <Burger isOpen={isOpen}>
+    <Line />
+    <Line />
+  </Burger>
+);
 
 // ============================================================================
 
 const Header = ({ siteName }) => {
   const [nav, toggleNav] = useState(false);
   const [contact, toggleContact] = useState(false);
+  const isOpen = !!(nav || contact);
 
   const data = useStaticQuery(graphql`
     query MyQuery {
@@ -31,21 +115,38 @@ const Header = ({ siteName }) => {
       }
     }
   `);
-
   const logo = data.allFile.edges.map(img => img.node.publicURL);
 
   return (
-    <StyledHeader>
+    <StyledHeader color={isOpen}>
       <Container display="flex">
-        <button type="submit" onClick={() => toggleContact(!contact)}>
-          {contact ? 'Close' : 'Contact'}
-        </button>
+        {!nav && (
+          <div>
+            <button type="submit" onClick={() => toggleContact(!contact)}>
+              <ContactButton isOpen={contact} />
+            </button>
+          </div>
+        )}
+
         {contact && <Contact />}
-        <img src={logo[0]} alt={siteName} />
+
+        {!contact && !nav ? (
+          <div>
+            <img src={logo[0]} alt={siteName} />
+          </div>
+        ) : (
+          ''
+        )}
+
         {nav && <Navigation />}
-        <button type="submit" onClick={() => toggleNav(!nav)}>
-          {nav ? 'Close' : 'Menu'}
-        </button>
+
+        {!contact && (
+          <div>
+            <button type="submit" onClick={() => toggleNav(!nav)}>
+              <NavigationButton isOpen={nav} />
+            </button>
+          </div>
+        )}
       </Container>
     </StyledHeader>
   );
@@ -65,12 +166,33 @@ const StyledHeader = styled.header`
     h-24
     md:h-32
   `}
+  transition: 350ms ease background-color;
+  ${props =>
+    props.color
+      ? 'background-color: var(--color-black-primary); color: var(--color-white-primary)'
+      : 'background-color: transparent'}
 `;
 
 // ============================================================================
 
 Header.propTypes = {
   siteName: PropTypes.string.isRequired,
+};
+
+ContactButton.propTypes = {
+  isOpen: PropTypes.bool,
+};
+
+ContactButton.defaultProps = {
+  isOpen: false,
+};
+
+NavigationButton.propTypes = {
+  isOpen: PropTypes.bool,
+};
+
+NavigationButton.defaultProps = {
+  isOpen: false,
 };
 
 // ============================================================================
