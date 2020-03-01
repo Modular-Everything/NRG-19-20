3719895573237347d2204cfd0b67af8775f7d4f2
Chrish Dunne <hey@chrish.design>
2020-03-01 18:53:09 +0000

    [~] Allow full width

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import tw from 'tailwind.macro';
 
-import Container from '../Container';
 import TempImg from '../../../static/images/video-temp.png';
 
 // ============================================================================
@@ -9,9 +8,7 @@ import TempImg from '../../../static/images/video-temp.png';
 const Video = () => {
   return (
     <div css={tw`mb-4`}>
-      <Container>
-        <img src={TempImg} alt="Video is on its way" />
-      </Container>
+      <img src={TempImg} alt="Video is on its way" />
     </div>
   );
 };
