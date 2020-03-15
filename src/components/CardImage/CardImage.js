8d2d611731c95cdb54bf6d41bd05f6f0f66843e6
Chrish Dunne <hey@chrish.design>
2020-03-15 15:04:37 +0000

    [~] Fix images not appearing full width

--- src/components/CardImage/CardImage.js
+++ src/components/CardImage/CardImage.js
@@ -9,7 +9,7 @@ const CardImage = props => {
   const { is: image } = props;
 
   const StyledImage = styled.img`
-    ${tw`rounded-t rounded-b-lg`};
+    ${tw`rounded-t rounded-b-lg w-full`};
   `;
 
   return (
@@ -28,60 +28,3 @@ CardImage.propTypes = {
 // ============================================================================
 
 export default CardImage;
-
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import tw from 'tailwind.macro';
-// import Img from 'gatsby-image';
-
-// import Container from '../Container';
-// import Caption from '../Caption';
-
-// // ============================================================================
-
-// const CardImage = props => {
-//   const { src: image, isHero, isCard, isStatic, caption } = props;
-
-//   const CardImg = () => <Img css={tw`rounded-t rounded-b-lg`} fluid={image} />;
-
-//   const HeroImg = () => (
-//     <Img css={tw`-mt-32 mb-4 h-screen sm:h-auto`} fluid={image} />
-//   );
-
-//   const StaticImg = () => (
-//     <Container>
-//       <Img css={tw`rounded mb-4`} fluid={image} />
-//       {caption && <Caption is={caption} />}
-//     </Container>
-//   );
-
-//   return (
-//     <>
-//       {isCard && <CardImg />}
-//       {isHero && <HeroImg />}
-//       {isStatic && <StaticImg />}
-//     </>
-//   );
-// };
-
-// // ============================================================================
-
-// Image.propTypes = {
-//   src: PropTypes.string,
-//   isHero: PropTypes.bool,
-//   isCard: PropTypes.bool,
-//   isStatic: PropTypes.bool,
-//   caption: PropTypes.string,
-// };
-
-// Image.defaultProps = {
-//   src: null,
-//   isHero: false,
-//   isCard: false,
-//   isStatic: false,
-//   caption: undefined,
-// };
-
-// // ============================================================================
-
-// export default CardImage;
