699e19e07853b95f0e256ca8bf1b0cce5e5924f7
Chrish Dunne <hey@chrish.design>
2020-04-05 12:30:51 +0100

    [~] Completely rewrote header code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,136 +1,36 @@
 import React, { useState } from 'react';
-import { Link, graphql, useStaticQuery } from 'gatsby';
-import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql, Link } from 'gatsby';
 import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+
 import tw from 'tailwind.macro';
 import styled from '@emotion/styled';
 
 import Container from '../Container';
-import Bolt from '../../../static/images/bolt.svg';
-
-// ============================================================================
-
-const Line = styled.span`
-  ${tw`h-px w-full block absolute`}
-  background-color: var(--color-black-primary);
-`;
-
-const Plus = styled.div`
-  ${tw`relative`}
-  width: 16px;
-  height: 16px;
-  ${props => props.isOpen && 'transform: rotate(225deg);'}
-
-  & span {
-    top: 8px;
-    ${props => props.isInverted && 'background: var(--color-white-primary)'}
-    ${props => props.isOpen && 'background: var(--color-white-primary)'}
-  }
-
-  & span:first-of-type {
-    transform: rotate(90deg);
-  }
-`;
-
-const Burger = styled.div`
-  ${tw`relative`}
-  width: 32px;
-  height: 32px;
-
-  & span {
-    top: 14px;
-    ${props => props.isInverted && 'background: var(--color-white-primary)'}
-    ${props => props.isOpen && 'background: var(--color-white-primary)'}
-  }
-
-  & span:last-of-type {
-    top: 22px;
-  }
-`;
-
-// ============================================================================
-
-const Contact = () => (
-  <>
-    <div>
-      <h3>Work with you</h3>
-      <a href="mailto:solutions@madewithnrg.com">
-        solutions
-        <br />
-        @madewithnrg.com
-      </a>
-    </div>
-    <div>
-      <h3>Work with us</h3>
-      <a href="mailto:talent@madewithnrg.com">
-        talent
-        <br />
-        @madewithnrg.com
-      </a>
-    </div>
-    <div>
-      <h3>Everything else</h3>
-      <a href="mailto:questions@madewithnrg.com">
-        questions
-        <br />
-        @madewithnrg.com
-      </a>
-    </div>
-  </>
-);
-const ContactButton = props => {
-  const { isOpen, isInverted } = props;
-  return (
-    <Plus isOpen={isOpen} isInverted={isInverted}>
-      <Line />
-      <Line />
-    </Plus>
-  );
-};
-
-const Navigation = () => (
-  <>
-    <div>
-      <img src={Bolt} alt="" />
-    </div>
-  </>
-);
-const NavigationButton = props => {
-  const { isOpen, isInverted } = props;
-  return (
-    <Burger isOpen={isOpen} isInverted={isInverted}>
-      <Line />
-      <Line />
-    </Burger>
-  );
-};
+import LogoLight from '../../../static/images/logo-light.svg';
+import LogoDark from '../../../static/images/logo-dark.svg';
 
 // ============================================================================
 
 const Header = props => {
   const { hasHero, isInverted, hasFade, name } = props;
-  const [nav, toggleNav] = useState(false);
-  const [contact, toggleContact] = useState(false);
-  const isOpen = !!(nav || contact);
 
   const data = useStaticQuery(graphql`
-    query MyQuery {
+    query MetaData {
       site {
         siteMetadata {
           title
         }
       }
-      allFile(filter: { name: { regex: "/logo/" }, ext: { eq: ".svg" } }) {
-        edges {
-          node {
-            publicURL
-          }
-        }
-      }
     }
   `);
+
   const SiteTitle = data.site.siteMetadata.title;
-  const logo = data.allFile.edges.map(img => img.node.publicURL);
+
+  const [mode, setMode] = useState({
+    contactOpen: false,
+    menuOpen: false,
+  });
 
   return (
     <>
@@ -141,71 +41,200 @@ const Header = props => {
         </title>
       </Helmet>
 
-      <StyledHeader color={isOpen ? 1 : 0} hasHero={hasHero} fade={hasFade}>
+      <HeaderBG
+        dark={mode.contactOpen || mode.menuOpen}
+        hasHero={hasHero}
+        hasFade={hasFade}
+        isInverted={isInverted}
+      >
         <Container display="flex">
-          {!nav && (
+          <div>
+            <button
+              type="button"
+              onClick={() => setMode({ contactOpen: !mode.contactOpen })}
+            >
+              {!mode.contactOpen ? `+` : `x`}
+            </button>
+          </div>
+
+          {!mode.contactOpen && !mode.menuOpen && (
             <div>
-              <button type="submit" onClick={() => toggleContact(!contact)}>
-                <ContactButton isOpen={contact} isInverted={isInverted} />
-              </button>
+              <img src={isInverted ? LogoLight : LogoDark} alt="NRG" />
             </div>
           )}
 
-          {contact && <Contact />}
-
-          {!contact && !nav ? (
-            <div>
-              <Link to="/">
-                <img src={logo[isInverted ? 1 : 0]} alt={SiteTitle} />
-              </Link>
-            </div>
-          ) : (
-            ''
+          {mode.contactOpen && !mode.menuOpen && (
+            <Contact>
+              <ul>
+                <li>
+                  <small>Work with you</small>
+                  <div>
+                    <a href="mailto:solutions@madewithnrg.com">
+                      solutions
+                      <span>@madewithnrg.com</span>
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <small>Work with us</small>
+                  <div>
+                    <a href="mailto:solutions@madewithnrg.com">
+                      solutions
+                      <span>@madewithnrg.com</span>
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <small>Everything else</small>
+                  <div>
+                    <a href="mailto:solutions@madewithnrg.com">
+                      solutions
+                      <span>@madewithnrg.com</span>
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </Contact>
           )}
 
-          {nav && <Navigation />}
-
-          {!contact && (
-            <div>
-              <button type="submit" onClick={() => toggleNav(!nav)}>
-                <NavigationButton isOpen={nav} isInverted={isInverted} />
-              </button>
-            </div>
+          {!mode.contactOpen && mode.menuOpen && (
+            <Navigation>
+              <ul>
+                <li>
+                  <Link
+                    to="/about"
+                    activeStyle={{ color: 'red' }}
+                    partiallyActive
+                  >
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/services"
+                    activeStyle={{ color: 'red' }}
+                    partiallyActive
+                  >
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/work"
+                    activeStyle={{ color: 'red' }}
+                    partiallyActive
+                  >
+                    Work
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/contact"
+                    activeStyle={{ color: 'red' }}
+                    partiallyActive
+                  >
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </Navigation>
           )}
+
+          <div>
+            <button
+              type="button"
+              onClick={() => setMode({ menuOpen: !mode.menuOpen })}
+            >
+              {!mode.menuOpen ? `=` : `x`}
+            </button>
+          </div>
         </Container>
-      </StyledHeader>
+      </HeaderBG>
     </>
   );
 };
 
+export default Header;
+
 // ============================================================================
 
-const StyledHeader = styled.header`
+const HeaderBG = styled.header`
   ${tw`
-    flex
-    flex-col
-    justify-center
-    relative
-    w-full
-    z-50
-    h-24
-    md:h-32
-  `}
-  ${({ fade }) =>
-    fade &&
-    `
-      background-image: linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0));
+    relative h-32 flex content-center z-50 font-sans
     `}
-  ${({ hasHero }) =>
-    hasHero
-      ? ''
-      : 'box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06); margin-bottom: 1rem;'}
-  ${({ color }) =>
-    color
-      ? 'background-color: var(--color-black-primary); color: var(--color-white-primary)'
-      : 'background-color: transparent'}
+
+  ${props =>
+    !props.hasHero &&
+    `box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06); margin-bottom: 1rem;'}`}
+
+  ${props => props.isInverted && `color: var(--color-white-primary);`}
+  
+  ${props =>
+    props.hasFade &&
+    `background-image: linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0));`}
+
+  ${props =>
+    props.dark &&
+    `
+      background-color: var(--color-black-primary);
+      color: var(--color-white-primary);
+  `}
 `;
 
+const Navigation = styled.div`
+  font-family: 'Simplon BP', -apple-system, 'Helvetica Neue', sans-serif;
+
+  & a {
+    &:hover {
+      color: var(--color-brand);
+    }
+  }
+
+  & ul {
+    ${tw`
+      flex flex-row text-2xl
+    `}
+
+    & li {
+      ${tw`mr-8`}
+
+      &:last-of-type {
+        ${tw`mr-0`}
+      }
+    }
+  }
+`;
+
+const Contact = styled.div`
+  font-family: 'Simplon BP', -apple-system, 'Helvetica Neue', sans-serif;
+
+  & small {
+    ${tw`text-xs`}
+  }
+
+  & a {
+    ${tw`leading-none`}
+    color: var(--color-brand);
+
+    & span {
+      ${tw`block`}
+    }
+  }
+
+  & ul {
+    ${tw`
+      flex flex-row text-2xl -mt-2
+    `}
+
+    & li {
+      ${tw`mr-12 leading-tight`}
+
+      &:last-of-type {
+        ${tw`mr-0`}
+      }
+    }
+  }
+`;
 // ============================================================================
 
 Header.propTypes = {
@@ -220,27 +249,3 @@ Header.defaultProps = {
   isInverted: false,
   hasFade: false,
 };
-
-ContactButton.propTypes = {
-  isOpen: PropTypes.bool,
-  isInverted: PropTypes.bool,
-};
-
-ContactButton.defaultProps = {
-  isOpen: false,
-  isInverted: false,
-};
-
-NavigationButton.propTypes = {
-  isOpen: PropTypes.bool,
-  isInverted: PropTypes.bool,
-};
-
-NavigationButton.defaultProps = {
-  isOpen: false,
-  isInverted: false,
-};
-
-// ============================================================================
-
-export default Header;
