cfabea448fa9396db7ab8b11216253036e09e7ca
Chrish Dunne <hey@chrish.design>
2020-04-06 17:07:46 +0100

    [+] Added Google Map

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,12 +31,5 @@ module.exports = {
         version: process.env.NODE_ENV === `production` ? `published` : `draft`,
       },
     },
-    {
-      resolve: `gatsby-source-googlemaps-static`,
-      options: {
-        key: `AIzaSyCWJEXadbLXqPLYd7eXVhXkfoKFmwzu4bs`,
-        center: `33.9919993,-118.4160509 || Los+Angeles,California`,
-      },
-    },
   ],
 };
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StaticGoogleMap, Marker } from 'react-static-google-map';
+
+import styled from '@emotion/styled';
+import tw from 'tailwind.macro';
 
 import Layout from '../Layout';
+import Container from '../Container';
 import Header from '../Header';
 import Footer from '../Footer';
-import MapImg from '../../../static/images/map.png';
+import Arrow from '../../../static/images/arrowWhite.svg';
 
 // ============================================================================
 
@@ -12,11 +17,71 @@ const Contact = () => {
   return (
     <Layout>
       <Header name="Contact" noGutter />
-      <img src={MapImg} alt="" />
+      <Map>
+        <a
+          href="https://goo.gl/maps/nodGFLUruHzDCsKV7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Overlay>
+            <Container>
+              <p>
+                Open in Maps
+                <span>
+                  <img src={Arrow} alt="->" />
+                </span>
+              </p>
+            </Container>
+          </Overlay>
+          <StaticGoogleMap
+            size="640x280"
+            className="img-fluid"
+            apiKey="AIzaSyCWJEXadbLXqPLYd7eXVhXkfoKFmwzu4bs"
+            zoom="13"
+            scale="4"
+            center="33.99771,-118.4222736"
+          >
+            <Marker location="33.99771,-118.4222736" scale="4" />
+          </StaticGoogleMap>
+        </a>
+      </Map>
+
       <Footer />
     </Layout>
   );
 };
+
+export default Contact;
+
+// ============================================================================
+
+const Map = styled.div`
+  ${tw`relative mb-12`}
+
+  & img {
+    width: 100%;
+  }
+`;
+
+const Overlay = styled.div`
+  ${tw`
+    h-32 absolute w-full bottom-0
+  `}
+
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 0.8)
+  );
+
+  & p {
+    ${tw`flex items-center text-white absolute bottom-0 mb-8 text-sm`}
+
+    & img {
+      ${tw`w-3 ml-2`}
+    }
+  }
+`;
 
 // ============================================================================
 
@@ -25,7 +90,3 @@ Contact.propTypes = {
     content: PropTypes.array.isRequired,
   }).isRequired,
 };
-
-// ============================================================================
-
-export default Contact;
