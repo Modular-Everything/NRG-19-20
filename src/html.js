92ee23ad14933b67e34f36727e9880aced636866
Chrish Dunne <hey@chrish.design>
2020-08-04 18:43:49 +0100

    [+] Added verification

--- src/html.js
+++ src/html.js
@@ -18,6 +18,10 @@ export default function HTML(props) {
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
+          name="google-site-verification"
+          content="cKE2wXs54iJ_xFBVkahYoKpisOuKGFxki81KwogS0uI"
+        />
+        <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
